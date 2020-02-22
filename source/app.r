ceb641c1dc122e7960151df431be9a001a5ed2ad
LanceQuitalig <laquitalig@up.edu.ph>
2020-02-22 17:59:12 +0800

    added all ui elements

--- source/app.r
+++ source/app.r
@@ -4,7 +4,20 @@ library(shiny)
 # UI function
 
 ui <- fluidPage(
-    
+    id = "main page",
+    title = "CMSC 128 Requirement",
+    sidebarLayout(
+        sidebarPanel(
+            h3("Welcome!"),
+            textOutput("status"),
+            br(),
+            textInput("user", "Username: ", "Username"),
+            textInput("pass", "Password: ", "Password"),
+            actionButton("signup", "Sign Up"),
+            actionButton("login", "Log In")
+        ),
+        mainPanel()
+    )
 ) 
 
 
